c219305277265a09ef5d0853b62b52e464422373
V N S Rama Krishna Pinnimty <ramakrishnapinnimty@gmail.com>
2022-01-16 20:08:12 -0500

    Uploaded Binary Search.

---
+++ DSA/Algorithms and Misc./Searching/BinarySearch.java
@@ -0,0 +1,45 @@
+public class BinarySearch {
+    // URL: https://www.programiz.com/java-programming/examples/binary-search
+
+    // I. Using recursion //
+    private static int binarySearchRecr(int[] arr, int num, int start, int end) {
+        if (start > end)
+            return -1;
+
+        int mid = (start + end) / 2;
+
+        if (arr[mid] < num)
+            return binarySearchRecr(arr, num, mid + 1, end);
+        else if (arr[mid] > num)
+            return binarySearchRecr(arr, num, start, mid - 1);
+
+        return mid;
+    }
+
+    // II. Using iteration //
+    private static int binarySearchIter(int[] arr, int num, int start, int end) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == num)
+                return mid;
+
+            if (arr[mid] < num)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int queryElement = 5;
+
+        int start = 0;
+        int end = arr.length - 1;
+        System.out.println(binarySearchRecr(arr, queryElement, start, end));
+        System.out.println(binarySearchIter(arr, queryElement, start, end));
+    }
+}
