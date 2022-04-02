c3e7b63410da23359f436ceea3edcfe125607d3a
V N S Rama Krishna Pinnimty <ramakrishnapinnimty@gmail.com>
2022-04-02 16:38:07 -0400

    Added functional recusrion to CheckPalindrome.

--- DSA/Algorithms and Misc./Recursion and Backtracking/CheckIfPalindrome.java
+++ DSA/Algorithms and Misc./Recursion and Backtracking/CheckIfPalindrome.java
@@ -19,8 +19,22 @@ public class CheckIfPalindrome {
         return str.equals(resStr);
     }
 
+    // Functional way
+    public static boolean isPalindromeFun(String str, int n, int idx) {
+        // If `idx` is either at midway or crosses it, then we have a palindrome string!
+        if (idx >= n / 2)
+            return true;
+
+        if (str.charAt(idx) != str.charAt(n - idx - 1))
+            return false;
+
+        return isPalindromeFun(str, n, idx + 1);
+    }
+
     public static void main(String[] args) {
         String str = "madam";
-        System.out.println("Is the given string a palindrome? : " + isPalindrome(str));
+        // System.out.println("Is the given string a palindrome? : " +
+        // isPalindrome(str));
+        System.out.println("Is the given string a palindrome? : " + isPalindromeFun(str, str.length(), 0));
     }
 }
